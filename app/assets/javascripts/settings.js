a0e464e543fb1429004699fe115164b0bcb8f915
Erik Lindebratt <erik.lindebratt@gmail.com>
2013-01-20 00:48:28 +0100

    Add some settings that might be used as part of RdioPlayer.

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -2,6 +2,12 @@ window.Settings = {
   soundcloud: {
     client_id: "3360f20ecd28b1992689f23391b50478",
     sdk_uri: "http://connect.soundcloud.com/sdk.js"
+  },
+  rdio: {
+    api_key: "cpz2nup8vgxgj2w4fupwx4tk",
+    swf_uri: "http://www.rdio.com/api/swf/",
+    playback_token: "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=",
+    domain: "localhost"
   }
 }
 
