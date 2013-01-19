423ecdec0b70e61709b40df7914770b9a7719d2b
Peter Lindkvist <peter.lindkvist@doberman.se>
2013-01-20 00:20:50 +0100

    added the spotify app

---
+++ doc/remote-api/js/Main.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var Main = function(){
+  var self = this;
+  this.sp = getSpotifyApi();
+  this.models = this.sp.require('$api/models');
+  window.view = this.views = this.sp.require('$api/models');
+  window.player = this._player = this.models.player;
+  this._channel_id = this.models.application.arguments[0];
+
+  this._player.observe(this.models.EVENT.CHANGE, function(event) {
+    self._onStateCallback(event);
+  });
+
+  $('.js-channel').html(this._channel_id);
+
+  this.pubnub = PUBNUB.init({
+    publish_key   : 'demo',
+    subscribe_key : 'demo'
+  });
+
+  this.pubnub.subscribe({
+    channel : "allplay_remote_api_client_" + self._channel_id,
+    message : function(message){
+      self._onCallback(message);
+    },
+    connect : function(channel) {
+      console.log("connect", channel);
+    }
+  })
+}
+
+Main.prototype._onStateCallback = function(state){
+  //console.log("state", state, this._player.position, this._player.track.duration);
+
+  this._send({
+    command:'onState',
+    data : state.data,
+    playing : this._player.playing,
+    position : this._player.position,
+    duration : this._player.track.duration
+  });
+}
+
+Main.prototype._send = function(message){
+  console.log("send", this._channel_id, message);
+  var self = this;
+  this.pubnub.publish({
+    channel : "allplay_remote_api_spotify_" + self._channel_id,
+    message : message
+  });
+}
+
+Main.prototype._onCallback = function(message){
+  console.log("message", message);
+  switch(message.command){
+    case "load":
+      this._player.play(message.url);
+      this._player.playing = false;
+      this._send({command : 'onload'});
+      break;
+    case "play":
+      this._player.playing = true;
+      break;
+    case "pause":
+      this._player.playing = false;
+      break;
+  }
+}
+
+window.onload = function() {
+  var main = new Main();
+}
