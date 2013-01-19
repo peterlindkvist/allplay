b87db638fddb37b6f41c0a774557f293c153a7ed
Peter Lindkvist <peter.lindkvist@doberman.se>
2013-01-19 12:24:13 +0100

    initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Allplay::Application.config.secret_token = 'fcb9e8b43789251e99b5b847f83991f33a82fa587bdea2ff86530d70c4430c7af72365dc2513213855f57c9f6ac49bd4ec729fc08f7f6c9b0bc5d48b5caef853'
