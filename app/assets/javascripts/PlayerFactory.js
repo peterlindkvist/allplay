5ca0c3d194f8de789a9034e16077ccbbc614457c
Olle Svensson <bolliolle@gmail.com>
2013-01-19 15:41:31 +0100

    basicplayer in factory

--- app/assets/javascripts/PlayerFactory.js
+++ app/assets/javascripts/PlayerFactory.js
@@ -3,6 +3,8 @@ PlayerFactory = function(){};
 PlayerFactory.resolve = function(url){
 	if(players.YoutubePlayer.isSupported(url)){
 		return new players.YoutubePlayer(url);
+	} else if (players.BasicPlayer.isSupported(url)){
+		return new players.BasicPlayer(url);
 	}
 	return new players.IPlayer(url);
 }
