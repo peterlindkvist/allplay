3e86d5536e75f34af5d6e76bcd794f76139f7835
Peter Lindkvist <peter.lindkvist@doberman.se>
2013-01-20 10:31:19 +0100

    release to heroku

--- release.sh
+++ release.sh
@@ -3,6 +3,7 @@ cp public/assets/application.js public/static/application.js
 cp public/assets/application.css public/static/application.css
 rake assets:clean
 
+git commit -am 'release to heroku'
 git pull
 git push
 git push heroku master
