21b1fae84e6e5c03318bb82e60252d43f7dee446
Peter Lindkvist <peter.lindkvist@doberman.se>
2013-01-20 10:19:40 +0100

    added friendly url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130119200401) do
+ActiveRecord::Schema.define(:version => 20130120090739) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20130119200401) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
 
   create_table "songs", :force => true do |t|
