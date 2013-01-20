f77b1b0b49cff6b30d44277a7b4f9c0977af9715
Erik Lindebratt <erik.lindebratt@gmail.com>
2013-01-20 16:10:09 +0100

    Update settings.js

---
+++ app/assets/javascripts/settings.js
@@ -0,0 +1,12 @@
+window.Settings = {
+  soundcloud: {
+    client_id: "",
+    sdk_uri: "http://connect.soundcloud.com/sdk.js"
+  },
+  rdio: {
+    client_id: "",
+    api_uri: "https://rdio.com/api/api.js?client_id=",
+    domain: "localhost"
+  }
+}
+
