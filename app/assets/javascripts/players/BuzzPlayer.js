1e66288488cefde3d681c33f7ee25789d8952d6b
Olle Svensson <bolliolle@gmail.com>
2013-01-20 09:41:03 +0100

    WIP droparea

--- app/assets/javascripts/players/BuzzPlayer.js
+++ app/assets/javascripts/players/BuzzPlayer.js
@@ -45,30 +45,9 @@ players.Buzz.getMetaData = function(url,callback){
 		  type: "html5Audio",
 	      author: "Missing data"
 	    };
-		if (/.*\.(wav|mp3|ogg|mp4)/.test(url)){
-		  var reader = new FileReader();
-		  reader.onload = function(e) {
-            var dv = new jDataView(this.result);
-       	    // "TAG" starts at byte -128 from EOF.
-			    // See http://en.wikipedia.org/wiki/ID3
-			if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
-			  var title = dv.getString(30, dv.tell());
-			  var artist = dv.getString(30, dv.tell());
-			  var album = dv.getString(30, dv.tell());
-			  var year = dv.getString(4, dv.tell());
-			} else {
-			      // no ID3v1 data found.
-			}
-		  };
-
-		  reader.readAsArrayBuffer(url);
-		  $.extend(trackData, {
-		  });
-		  callback(trackData);
-		}else {
-		  $.extend(trackData, {});
-		    callback(trackData);
-		}
+	    $.extend(trackData, {});
+	    callback(trackData);
+		
 	}, false);
 }
 
